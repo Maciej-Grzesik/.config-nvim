d15e26dc48ca76caf62df322ffc7422a5b94b12c
Maciej-Grzesik <mackeplz61@gmail.com>
2025-03-18 21:47:57 +0100

    init of nvim config

---
+++ lua/configs/lspconfig.lua
@@ -0,0 +1,37 @@
+-- load defaults i.e lua_lsp
+require("nvchad.configs.lspconfig").defaults()
+
+local lspconfig = require "lspconfig"
+
+-- EXAMPLE
+local servers = { "html", "cssls", "jdtls" }
+local nvlsp = require "nvchad.configs.lspconfig"
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
+ 
+lspconfig.jdtls.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  cmd = { "jdtls" }, -- Upewnij się, że masz jdtls w PATH
+  root_dir = lspconfig.util.root_pattern("pom.xml", "gradle.build", ".git"),
+  settings = {
+    java = {
+      signatureHelp = { enabled = true },
+      contentProvider = { preferred = "fernflower" },
+    },
+  },
+}
+-- configuring single server, example: typescript
+-- lspconfig.ts_ls.setup {
+--   on_attach = nvlsp.on_attach,
+--   on_init = nvlsp.on_init,
+--   capabilities = nvlsp.capabilities,
+-- }
---
+++ lua/plugins/init.lua
@@ -0,0 +1,110 @@
+return {
+  {
+    "andweeb/presence.nvim",
+    lazy=false,
+    config = function ()
+      require("presence").setup({
+        auto_update = true,
+        show_time = true,
+        main_image = "neovim",
+        neovim_image_text="dog dancing",
+        enable_line_number = true,
+        buttons = true,
+        debounce_timeout=10,
+      })
+    end
+  },
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = {
+      view = {
+        adaptive_size = true,
+      }
+    }
+  },
+
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^5', -- Recommended
+    lazy = false, -- This plugin is already lazy
+
+    config = function ()
+      local mason_registry = require('mason-registry')
+      local codelldb = mason_registry.get_package("codelldb")
+      local extension_path = codelldb:get_install_path().."/extension"
+      local codelldb_path = extension_path .. "adapter/codelldb"
+      local liblldb_path = extension_path  .. "lldb/lib/liblldb.dylib"
+      local cfg = require('rustaceanvim.config')
+
+      vim.g.rustaceanvim = {
+        dap = {
+          adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+        },
+      }
+    
+    end
+  },
+
+  {
+    'mfussenegger/nvim-dap',
+    config = function ()
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.before.attach.dapui_config = function ()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function ()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function ()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function ()
+        dapui.close()
+      end
+    end
+  },
+
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+      config = function ()
+        require("dapui").setup()
+      end,
+    }
+  },
+ 
+  {
+    "okuuva/auto-save.nvim",
+    version = '^1.0.0', -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
+    cmd = "ASToggle", -- optional for lazy loading on command
+    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+    opts = {
+      -- your config goes here
+      -- or just leave it empty :)
+    },
+ }, -- {
+   -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
